915adcd9b5055f3b6dac1cfacf77bbe4fa6ce715
Mihail Preis <mihail.preis@waveaccess.ru>
2019-04-28 00:44:30 +0500

    implement minimal tracking flow

--- functions.js
+++ functions.js
@@ -1,20 +1,21 @@
 const psList = require('ps-list');
 const fs = require('fs');
 const pt = require('path');
+const moment = require('moment');
 const { remote } = require('electron');
 const dialog = remote.dialog;
 const configName = './config.json';
-let config;
+let timer;
 
 function loadConfig() {
-    if (config) return;
+    if (window.config) return;
     try {
-        config = require(configName);
+        window.config = require(configName);
         console.log("Config is loaded!")
     } catch (err) {
         if (err.code === "MODULE_NOT_FOUND") {
-            config = {handlingProcesses: []};
-            saveConfig()
+            window.config = {handlingProcesses: [], timeoutProcessHandling: 60000, trackData: []};
+            saveConfig(true);
         }
         else {
             alert(err);
@@ -23,20 +24,27 @@ function loadConfig() {
     }
 }
 
-function saveConfig() {
-    fs.writeFile(configName, JSON.stringify(config), function (err) {
-        if (err) return console.log(err);
-        console.log(JSON.stringify(config));
-        console.log('writing to ' + configName);
-        config = require(configName);
-    });
+function saveConfig(isSync = false) {
+    if (isSync) {
+        try {
+            fs.writeFileSync(configName, JSON.stringify(window.config));
+            window.config = require(configName);
+        } catch (err) {
+            console.log(err);
+        }
+    } else {
+        fs.writeFile(configName, JSON.stringify(window.config), function (err) {
+            if (err) return console.log(err);
+            window.config = require(configName);
+        });
+    }
 }
 
 function refreshTable() {
     let table = $("tbody#process-table");
     table.empty();
 
-    config.handlingProcesses.forEach((value, index) => {
+    window.config.handlingProcesses.forEach((value, index) => {
         let row = `<tr>
                 <th scope=\"row\">${index + 1}</th>
                 <td>${value}</td>
@@ -50,57 +58,144 @@ function refreshTable() {
     })
 }
 
-function addProcess(path) {
-    let name = pt.basename(path, ".app");
-    if (config.handlingProcesses.includes(name)) return;
-    config.handlingProcesses.push(name);
+function clearAllStoreData() {
+    window.config.trackData = [];
+    console.log(window.config.trackData);
+    if (window.chart !== undefined) {
+        window.chart.data = [];
+        console.log(window.chart.data);
+        window.chart.invalidateData();
+    }
+    saveConfig()
+}
+
+function showAddProcessesDialog() {
+    let extensions = ['app', 'exe'];
+    if (process.platform === 'linux') extensions.push("*");
+    dialog.showOpenDialog (
+        remote.getCurrentWindow (),
+        {
+            properties: ["openFile"],
+            filters: [
+                { name: 'Apps', extensions: extensions }
+            ]
+        },
+        filePaths => {
+            if (!filePaths) return;
+            filePaths.forEach(file => {
+                let name = pt.basename(file, ".app");
+                if (window.config.handlingProcesses.includes(name)) return;
+                window.config.handlingProcesses.push(name);
+                saveConfig();
+                refreshTable()
+            });
+        }
+    );
+}
+
+function deleteProcess(button) {
+    let index = parseInt($(button).attr('id').slice(4));
+    window.config.handlingProcesses.splice(index, 1);
     saveConfig();
     refreshTable()
 }
 
-window.deleteProcess = function (button) {
-    let index = parseInt($(button).attr('id').slice(4));
-    config.handlingProcesses.splice(index, 1);
-    saveConfig();
-    refreshTable()
-};
+window.deleteProcess = deleteProcess;
+
+function updateTimeoutProcessHandling(value) {
+    if (typeof value == "string") {
+        value = parseInt(value)
+    }
+    if (value > 1000) {
+        value = value / 1000;
+    }
+    $('input#currentTimeoutRange').val(value);
+    $('span#currentTimeout').text(value)
+}
 
 document.addEventListener('DOMContentLoaded', function() {
 
     loadConfig();
     refreshTable();
+    updateTimeoutProcessHandling(window.config.timeoutProcessHandling);
+    initTimer();
 
-    $('button#add-processes').click(function () {
-        dialog.showOpenDialog (
-            remote.getCurrentWindow (),
-            {
-                properties: ["openFile"],
-                filters: [
-                    { name: 'Apps', extensions: ['app', 'exe'] }
-                ]
-            },
-            filePaths => {
-                filePaths.forEach(file => {
-                    addProcess(file)
-                });
-            }
-        );
+    $('input#currentTimeoutRange').change(function() {
+        let value = $(this).val();
+        if (typeof value == "string") {
+            value = parseInt(value)
+        }
+        window.config.timeoutProcessHandling = value * 1000;
+        initTimer();
+        updateTimeoutProcessHandling(value);
+        saveConfig()
     });
 
-    $('#exampleModalLong').on('show.bs.modal', function () {
-        psList().then(
-            result => {
-                $("p#p1").empty();
-                result.filter(item => {
-                        return config.handlingProcesses.includes(item.name)
-                    }).map(item => {
-                        return `${item.pid} - ${item.name}`
-                    }).forEach(item => {
-                        $("p#p1").append(`<p>${item}</p>`)
-                    })
-            },
-            error => alert(error)
-        );
-    })
+    $('button#add-processes').click(function () {
+        showAddProcessesDialog()
+    });
+
+    $('button#clear-all').click(function () {
+        clearAllStoreData()
+    });
 
 }, false);
+
+function initTimer() {
+    if (timer) clearInterval(timer);
+    timer = setInterval(function() {
+        checkSelectedProcesses()
+    }, window.config.timeoutProcessHandling);
+}
+
+function checkSelectedProcesses() {
+    psList().then(
+        result => {
+            let handlingProcesses = [];
+
+            result.forEach(item => {
+                if (window.config.handlingProcesses.includes(item.name)
+                    && !handlingProcesses.includes(item.name)) {
+                    handlingProcesses.push(item.name);
+                }
+            });
+
+            handlingProcesses.forEach(item => {
+                let longDateFormat  = 'YYYY-MM-DD HH:mm:ss';
+                let curDate = new Date();
+                let currentIndex = null;
+
+                let data = window.config.trackData.find((value, index) => {
+                    let lastDate = moment(value.toDate, longDateFormat).toDate();
+                    let result = value.name === item && curDate - lastDate <= window.config.timeoutProcessHandling * 2;
+                    if (result) currentIndex = index;
+                    return result
+                });
+
+                if (data === undefined) {
+                    data = {
+                        name: item,
+                        fromDate: moment(curDate).format(longDateFormat),
+                        toDate: moment(curDate).add(1, 's').format(longDateFormat)
+                    }
+                } else {
+                    data.toDate = moment(curDate).format(longDateFormat)
+                }
+
+                if (currentIndex === null) {
+                    window.config.trackData.push(data);
+                } else {
+                    window.config.trackData[currentIndex] = data;
+                }
+
+                if (window.chart !== undefined) {
+                    window.chart.data = setColors(window.config.trackData);
+                    window.chart.invalidateData();
+                }
+            });
+
+            saveConfig()
+        },
+        error => alert(error)
+    );
+}
